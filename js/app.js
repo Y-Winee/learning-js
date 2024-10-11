fffb7070a5e2a0dac250855009e717b3039de047
Y-Winee <winehtethein22@gmail.com>
2024-10-11 13:04:08 +0630

    adding new changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 const btn = document.querySelector("#nav-btn");
 const section = document.querySelector("#nav-sec");
+const lists = document.querySelectorAll(".custom");
+const showResult = document.querySelector("#result");
+const google = document.querySelector("#google");
+const yt = document.querySelector("#youtube");
+
+google.setAttribute("href", "https://www.youtube.com");
+google.removeAttribute("target");
 
 btn.onclick = () => {
     section.classList.toggle("translate-x-full");
 };
+
+lists.forEach((list) => {  
+    list.onclick = () => {
+        list.classList.toggle("bg-gray-400");
+        const countSelected = document.querySelectorAll(".custom.bg-gray-400");
+        showResult.innerText = countSelected.length;
+    };
+})
