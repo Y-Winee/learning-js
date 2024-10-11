5a60d62b2a94ec496bedb92b2e1d5be3b57a8516
Y-Winee <winehtethein22@gmail.com>
2024-10-11 17:40:33 +0630

    adding remove

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,5 @@
 const root = document.querySelector("#root");
-root.classList.add("p-4")
+root.classList.add("p-4", "space-y-3")
 
 //create html elements
 const heading = document.createElement("h1");
@@ -7,6 +7,19 @@ heading.innerText = "Hello World";
 
 const para = document.createElement("p");
 para.innerText = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id odio, porro harum dignissimos, provident omnis labore, perspiciatis molestias veniam rerum voluptates nostrum voluptatem. Sit, nam sequi? Repudiandae vel dolore quibusdam!"
+
+const ul = document.createElement("ul");
+
+const createLi = (text) => {
+    const li = document.createElement("li");
+    li.classList.add("border", "p-2")
+    li.innerText = text;
+    return li;
+}
+ul.append(createLi("apple"));
+ul.append(createLi("mango"));
+ul.append(createLi("banana"));
+
 
 // adding attributes to elements
 heading.title = "Greeting";
@@ -16,4 +29,8 @@ heading.classList.add("text-3xl", "text-gray-700", "font-bold", "font-serif");
 
 //append elements to root
 root.append(heading);
-root.append(para);
+heading.after(para);
+root.append(ul);
+
+//removing
+ul.removeChild(ul.children[1])
